9d2c367a48fc13fac2089f1eec69cf1bca6a0527
daljeet singh <singhdaljeet8637@gmail.com>
2025-09-06 22:04:50 +0530

    final commit

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -19,7 +19,7 @@ export default function Hero() {
   return (
     <>
       <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 items-center min-h-[90vh] px-4 sm:px-8 lg:px-12 pt-24">
-        {/* Left Content */}
+       
         <motion.div
           initial={{ opacity: 0, x: -50 }}
           whileInView={{ opacity: 1, x: 0 }}
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,10 +2,14 @@ import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Scrollbutton from "./components/Scrollbutton";
+import { Icon, icons } from "lucide-react";
 
 export const metadata = {
-  title: "My Website",
+  title: "My Portfolio",
   description: "Next.js website with Navbar",
+  icons: {
+    Icon:"/images/favicon.ico"
+  }
 };
 
 export default function RootLayout({ children }) {
